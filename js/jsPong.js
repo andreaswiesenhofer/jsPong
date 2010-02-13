2d4933c9936e42b367c47c35919f2095735a4be2
Andreas Wiesenhofer <awiesi@gmail.com>
2010-02-13 19:06:13 +0100

    JSLint-ed

--- js/jsPong.js
+++ js/jsPong.js
@@ -6,13 +6,13 @@
 var jsPong = {
 
 	stage: {
-		object: null,
+		object: {},
 		height: null,
 		width: null
 	},
 	
 	ball: {
-		sprite: null,
+		sprite: {},
 		x: null,
 		y: null,
 		speed: {
@@ -29,11 +29,11 @@ var jsPong = {
 	pW: 150,
 	
 	human: {
-		sprite: null,
+		sprite: {},
 		x: null,
 		y: null,
 		score: null,
-		scoreboard: null
+		scoreboard: {}
 	},
 	
 	computer: {
@@ -41,7 +41,7 @@ var jsPong = {
 		x: null,
 		y: null,
 		score: null,
-		scoreboard: null
+		scoreboard: {}
 	},
 	
 	intMainLoop: 0,
@@ -53,13 +53,16 @@ var jsPong = {
 	** @return {string} number with leading zero
 	*/
 	fmt00: function (XtoFormat){
- 		
- 		if (parseInt(XtoFormat) < 0) var neg = true;
- 		
-		if (Math.abs(parseInt(XtoFormat)) < 10){
-			XtoFormat = "0"+ Math.abs(XtoFormat);
-		}
-		if (neg) XtoFormat = "-"+XtoFormat;
+		
+		var neg = false;
+		
+		if (parseInt(XtoFormat, 10) < 0) { neg = true; }
+		
+		if (Math.abs(parseInt(XtoFormat,10)) < 10)
+			{ XtoFormat = "0"+ Math.abs(XtoFormat); }
+			
+		if (neg) { XtoFormat = "-"+XtoFormat; }
+		
 		return XtoFormat;
 	},
 	
@@ -78,47 +81,47 @@ var jsPong = {
 	
 		/* Create and Style Ball-Sprite */
 		jsPong.ball.sprite = document.createElement("div");
-		jsPong.ball.sprite.style['position'] = 'absolute';
-		jsPong.ball.sprite.style['background'] = 'white';
-		jsPong.ball.sprite.style['left'] = jsPong.stage.width / 2 + 'px';
-		jsPong.ball.sprite.style['top'] = jsPong.stage.height / 2 + 'px';	
-		jsPong.ball.sprite.style['width'] = jsPong.grid + 'px';
-		jsPong.ball.sprite.style['height'] = jsPong.grid + 'px';
-		jsPong.ball.sprite.style['z-index'] = '15';
+		jsPong.ball.sprite.style.position = 'absolute';
+		jsPong.ball.sprite.style.background = 'white';
+		jsPong.ball.sprite.style.left = jsPong.stage.width / 2 + 'px';
+		jsPong.ball.sprite.style.top = jsPong.stage.height / 2 + 'px';	
+		jsPong.ball.sprite.style.width = jsPong.grid + 'px';
+		jsPong.ball.sprite.style.height = jsPong.grid + 'px';
+		jsPong.ball.sprite.style.zindex = '15';
 	
 		/* Create and Style Racket of Human Player */
 		jsPong.human.sprite = document.createElement("div");
-		jsPong.human.sprite.style['position'] = 'absolute';
-		jsPong.human.sprite.style['background'] = 'white';
-		jsPong.human.sprite.style['left'] = jsPong.human.x + 'px';
-		jsPong.human.sprite.style['top'] = jsPong.human.y + 'px';	
-		jsPong.human.sprite.style['width'] = jsPong.pW + 'px';
-		jsPong.human.sprite.style['height'] = jsPong.grid + 'px';
-		jsPong.human.sprite.style['z-index'] = '15';
+		jsPong.human.sprite.style.position = 'absolute';
+		jsPong.human.sprite.style.background = 'white';
+		jsPong.human.sprite.style.left = jsPong.human.x + 'px';
+		jsPong.human.sprite.style.top = jsPong.human.y + 'px';	
+		jsPong.human.sprite.style.width = jsPong.pW + 'px';
+		jsPong.human.sprite.style.height = jsPong.grid + 'px';
+		jsPong.human.sprite.style.zindex = '15';
 	
 		/* Create and Style Scoreboard of Human Player */
 		jsPong.human.scoreboard = document.createElement("div");
-		jsPong.human.scoreboard.style['position'] = 'absolute'; 
-		jsPong.human.scoreboard.style['right'] = '0px';
-		jsPong.human.scoreboard.style['bottom'] = '23px';	
+		jsPong.human.scoreboard.style.position = 'absolute'; 
+		jsPong.human.scoreboard.style.right = '0px';
+		jsPong.human.scoreboard.style.bottom = '23px';	
 		jsPong.human.scoreboard.className = 'scoreboard';
 		jsPong.human.scoreboard.id = 'human_scoreboard';
 		
 		/* Create and Style Racket of Computer Player */
 		jsPong.computer.sprite = document.createElement("div");
-		jsPong.computer.sprite.style['position'] = 'absolute';
-		jsPong.computer.sprite.style['background'] = 'white';
-		jsPong.computer.sprite.style['left'] = jsPong.computer.x + 'px';
-		jsPong.computer.sprite.style['top'] = jsPong.computer.y + 'px';	
-		jsPong.computer.sprite.style['width'] = jsPong.pW + 'px';
-		jsPong.computer.sprite.style['height'] = jsPong.grid + 'px';
-		jsPong.computer.sprite.style['z-index'] = '15';
+		jsPong.computer.sprite.style.position = 'absolute';
+		jsPong.computer.sprite.style.background = 'white';
+		jsPong.computer.sprite.style.left = jsPong.computer.x + 'px';
+		jsPong.computer.sprite.style.top = jsPong.computer.y + 'px';	
+		jsPong.computer.sprite.style.width = jsPong.pW + 'px';
+		jsPong.computer.sprite.style.height = jsPong.grid + 'px';
+		jsPong.computer.sprite.style.zindex = '15';
 	
 		/* Create and Style Scoreboard of Computer Player */
 		jsPong.computer.scoreboard = document.createElement("div");
-		jsPong.computer.scoreboard.style['position'] = 'absolute'; 
-		jsPong.computer.scoreboard.style['left'] = '0px';
-		jsPong.computer.scoreboard.style['top'] = '28px';	
+		jsPong.computer.scoreboard.style.position = 'absolute'; 
+		jsPong.computer.scoreboard.style.left = '0px';
+		jsPong.computer.scoreboard.style.top = '28px';	
 		jsPong.computer.scoreboard.className = 'scoreboard';
 		jsPong.computer.scoreboard.id = 'computer_scoreboard';
 		
@@ -137,18 +140,21 @@ var jsPong = {
 	}, /* init() */
 
 	oKeyDown: function (e){
-		var ev = e ? e.keyCode : event.keyCode  
+		var ev;
+		
+		if(e) { ev = e.keyCode; } else { ev = event.keyCode; }  
+		
 		if(ev == 37){ jsPong.kLeft = true;	}
 			else if(ev == 39){ jsPong.kRight = true;	}
 			else if(ev == 80 || ev == 27){ //letter p, spacebar=32
 				if(jsPong.vIsPause === false) {
 					jsPong.vIsPause = true;
-					jsPong.human.sprite.style['background'] = jsPong.computer.sprite.style['background'] = jsPong.ball.sprite.style['background'] = 'gray';
+					jsPong.human.sprite.style.background = jsPong.computer.sprite.style.background = jsPong.ball.sprite.style.background = 'gray';
 					getElementById('game_menu').style.visibility='visible';
 				}
 				else {
 					jsPong.vIsPause = false;
-					jsPong.human.sprite.style['background'] = jsPong.computer.sprite.style['background'] = jsPong.ball.sprite.style['background'] = 'white';
+					jsPong.human.sprite.style.background = jsPong.computer.sprite.style.background = jsPong.ball.sprite.style.background = 'white';
 					getElementById('game_menu').style.visibility='hidden';
 				}
 			}
@@ -188,17 +194,17 @@ var jsPong = {
 	
 			//Move ball
 			jsPong.ball.x+=jsPong.ball.speed.x; jsPong.ball.y+=jsPong.ball.speed.y;	
-			jsPong.ball.sprite.style['left'] = jsPong.ball.x + 'px';
-			jsPong.ball.sprite.style['top'] = jsPong.ball.y + 'px';
+			jsPong.ball.sprite.style.left = jsPong.ball.x + 'px';
+			jsPong.ball.sprite.style.top = jsPong.ball.y + 'px';
 	
 			//Move Player1
 			if(jsPong.kLeft && jsPong.human.x > 0){ jsPong.human.x-=5; } else if (jsPong.kRight && jsPong.human.x+jsPong.pW-jsPong.grid < jsPong.stage.width){ jsPong.human.x+=5;}
-			jsPong.human.sprite.style['left'] = jsPong.human.x + 'px';
+			jsPong.human.sprite.style.left = jsPong.human.x + 'px';
 	
 			//Move Player2
 			if(jsPong.ball.x > jsPong.computer.x+jsPong.pW){ jsPong.computer.x+=5; } else if(jsPong.ball.x < jsPong.computer.x){ jsPong.computer.x-=5; }
-			jsPong.computer.sprite.style['left'] = jsPong.computer.x + 'px';
+			jsPong.computer.sprite.style.left = jsPong.computer.x + 'px';
 		}
 	} /* fnLoop() */
 
-} /* End jsPong */
+}; /* End jsPong */
